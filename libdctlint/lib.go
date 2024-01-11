Domain-Connect/dc-template-linter

commit 46e60c3f3a2e073ceb29316c61ce1c6a0a3117f1

allow use of dc-template-linter as a library
The point of this change is to make libdctlint.CheckTemplate() function to
be easy to integrate with other projects. When time allows this project will
include an example usage of the library.

Signed-off-by: Sami Kerola <kerolasa@iki.fi>

libdctlint/lib.go
@@ -15,7 +15,7 @@ import (
 	"path"
 	"strings"
 
-	"github.com/Domain-Connect/dc-template-linter/libdctlint/internal"
+	"github.com/Domain-Connect/dc-template-linter/internal"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/rs/zerolog"
@@ -75,13 +75,9 @@ func NewConf(checkLogos, cloudflare, inplace, prettyPrint bool) Conf {
 	}
 }
 
-// CheckTemplate takes bufio.Reader as an argument and will run template
-// checks according to the Conf configuration. Please remember to
-// set conf.FileName appropriately before calling this function to avoid
-// confusing results.
-func (conf *Conf) CheckTemplate(f *bufio.Reader) CheckSeverity {
-	// A single template check init
-	exitVal := CheckOK
+// GetAndCheckTemplate is used in dctweb. Do not use applications
+// outside of this project.
+func (conf *Conf) GetAndCheckTemplate(f *bufio.Reader) (internal.Template, CheckSeverity) {
 	conf.tlog = log.With().Str("template", conf.FileName).Logger()
 	internal.SetLogger(conf.tlog)
 	conf.tlog.Debug().Msg("starting template check")
@@ -91,12 +87,30 @@ func (conf *Conf) CheckTemplate(f *bufio.Reader) CheckSeverity {
 	decoder.DisallowUnknownFields()
 	var template internal.Template
 	err := decoder.Decode(&template)
-	exitVal |= CheckSeverity(internal.GetUnmarshalStatus())
+	exitVal := CheckSeverity(internal.GetUnmarshalStatus())
 	if err != nil {
 		conf.tlog.Error().Err(err).Msg("json decode error")
-		return exitVal | CheckFatal
+		return template, CheckFatal
 	}
+	exitVal |= conf.checkTemplate(f, template)
+	return template, exitVal
+}
 
+// CheckTemplate takes bufio.Reader as an argument and will run template
+// checks according to the Conf configuration. Please remember to
+// set conf.FileName appropriately before calling this function to avoid
+// confusing results.
+func (conf *Conf) CheckTemplate(f *bufio.Reader) CheckSeverity {
+	// A single template check init
+	template, exitVal := conf.GetAndCheckTemplate(f)
+	if exitVal == CheckFatal {
+		return CheckFatal
+	}
+	return conf.checkTemplate(f, template)
+}
+
+func (conf *Conf) checkTemplate(f *bufio.Reader, template internal.Template) CheckSeverity {
+	exitVal := CheckOK
 	// Ensure ID fields use valid characters
 	if checkInvalidChars(template.ProviderID) {
 		conf.tlog.Error().Str("providerId", template.ProviderID).Msg("providerId contains invalid characters")
@@ -119,7 +133,7 @@ func (conf *Conf) CheckTemplate(f *bufio.Reader) CheckSeverity {
 
 	// Check 'validate:' fields in internal/json.go definitions
 	Validator := validator.New()
-	err = Validator.Struct(template)
+	err := Validator.Struct(template)
 	if err != nil {
 		for _, err := range err.(validator.ValidationErrors) {
 			conf.tlog.Warn().Err(err).Msg("template field validation")

version.go
@@ -1,3 +1,3 @@
 package main
 
-const dcTemplateLinterVersion uint = 18
+const dcTemplateLinterVersion uint = 19

internal/json.go
@@ -61,9 +61,9 @@ type SINT int
 // recovered while causing warning.
 var exitVal int
 
-// log is similar side channel to exitVal but for json parser extra input
-// rather than output.
-var log zerolog.Logger
+// smuggledLog is similar side channel to exitVal but for json parser extra
+// input rather than output.
+var smuggledLog zerolog.Logger
 
 // lock will ensure only one exitVal / logger pair is in use.
 var mu sync.Mutex
@@ -98,14 +98,14 @@ func (sint *SINT) UnmarshalJSON(b []byte) error {
 		return err
 	}
 	exitVal = 1
-	log.Warn().Str("value", s).Msg("do not quote an integer, it makes it string")
+	smuggledLog.Warn().Str("value", s).Msg("do not quote an integer, it makes it string")
 	*sint = SINT(i)
 	return nil
 }
 
 func SetLogger(l zerolog.Logger) {
 	mu.Lock()
-	log = l
+	smuggledLog = l
 }
 
 func GetUnmarshalStatus() int {

internal/zerolog.go
@@ -1,0 +1,14 @@
+package internal
+
+import (
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func SetLoglevel(loglevel string) {
+	level, err := zerolog.ParseLevel(loglevel)
+	if err != nil {
+		log.Fatal().Err(err).Msg("invalid loglevel")
+	}
+	zerolog.SetGlobalLevel(level)
+}

main.go
@@ -15,20 +15,13 @@ import (
 	"os"
 	"time"
 
+	"github.com/Domain-Connect/dc-template-linter/internal"
 	"github.com/Domain-Connect/dc-template-linter/libdctlint"
 
 	"github.com/mattn/go-isatty"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
-
-func setLoglevel(loglevel string) {
-	level, err := zerolog.ParseLevel(loglevel)
-	if err != nil {
-		log.Fatal().Err(err).Msg("invalid loglevel")
-	}
-	zerolog.SetGlobalLevel(level)
-}
 
 func main() {
 	// Init logging. Essentially colors or no colors?
@@ -64,7 +57,7 @@ func main() {
 	}
 
 	// Runtime init
-	setLoglevel(*loglevel)
+	internal.SetLoglevel(*loglevel)
 	exitVal := libdctlint.CheckOK
 
 	if flag.NArg() < 1 {

commit b7fbd8c9f9a18789b540727c0f218611f8924beb

do not process templates twice
Change that provided a parsed template as a function GetAndCheckTemplate()
output started checking template validity, which means CheckTemplate() does not
need to do that action again.

Fixes: 46e60c3f3a2e073ceb29316c61ce1c6a0a3117f1
Signed-off-by: Sami Kerola <kerolasa@iki.fi>

libdctlint/lib.go
@@ -102,11 +102,8 @@ func (conf *Conf) GetAndCheckTemplate(f *bufio.Reader) (internal.Template, Check
 // confusing results.
 func (conf *Conf) CheckTemplate(f *bufio.Reader) CheckSeverity {
 	// A single template check init
-	template, exitVal := conf.GetAndCheckTemplate(f)
-	if exitVal == CheckFatal {
-		return CheckFatal
-	}
-	return conf.checkTemplate(f, template)
+	_, exitVal := conf.GetAndCheckTemplate(f)
+	return exitVal
 }
 
 func (conf *Conf) checkTemplate(f *bufio.Reader, template internal.Template) CheckSeverity {

version.go
@@ -1,3 +1,3 @@
 package main
 
-const dcTemplateLinterVersion uint = 21
+const dcTemplateLinterVersion uint = 22