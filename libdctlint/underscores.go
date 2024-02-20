Domain-Connect/dc-template-linter

commit 0bac87ecf2bddd795610176ff3cabb7fe1707878

allow _domainkey to use CNAME, although it's not part of global spec
According to wikipedia 'A CNAME record can also be used to point at a
different TXT record, for example when one organization sends email on
behalf of another'.  This is also defined in RFC5016 sectio 5.3 bullet 6.
That means IANA dns-parameters are a bit out of date.

Reported-by: Pawel Kowalik
Reference: https://github.com/Domain-Connect/dc-template-linter/issues/3
Reference: https://en.wikipedia.org/wiki/DomainKeys_Identified_Mail#Verification
Reference: https://www.rfc-editor.org/rfc/rfc5016#section-5.3
Signed-off-by: Sami Kerola <kerolasa@iki.fi>

libdctlint/underscores.go
@@ -7,6 +7,7 @@ import (
 )
 
 const (
+	TypeCNAME      uint16 = 5
 	TypeNULL       uint16 = 10
 	TypeTXT        uint16 = 16
 	TypeSRV        uint16 = 33
@@ -37,7 +38,7 @@ var rfc8552 = map[string][]uint16{
 	"_dccp":                    {TypeSRV, TypeURI},
 	"_dmarc":                   {TypeTXT},
 	"_dns":                     {TypeSVCB},
-	"_domainkey":               {TypeTXT},
+	"_domainkey":               {TypeTXT, TypeCNAME},
 	"_email":                   {TypeURI},
 	"_ems":                     {TypeURI},
 	"_fax":                     {TypeURI},

version.go
@@ -1,3 +1,3 @@
 package main
 
-const dcTemplateLinterVersion uint = 25
+const dcTemplateLinterVersion uint = 26

commit 57e9d269a5cf68f0a51178d607b5c7bbe8b42b88

fix _domainkey to CNAME template parsing
Earlier commit missed a string to type definition.

Fixes: 0bac87ecf2bddd795610176ff3cabb7fe1707878
Signed-off-by: Sami Kerola <kerolasa@iki.fi>

libdctlint/underscores.go
@@ -20,6 +20,7 @@ const (
 )
 
 var recordToType = map[string]uint16{
+	"CNAME":      TypeCNAME,
 	"NULL":       TypeNULL,
 	"TXT":        TypeTXT,
 	"SRV":        TypeSRV,

version.go
@@ -1,3 +1,3 @@
 package main
 
-const dcTemplateLinterVersion uint = 26
+const dcTemplateLinterVersion uint = 27