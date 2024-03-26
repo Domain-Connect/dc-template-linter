97944df047bd91fe4872ad6ee1472d14bbd61e33
Sami Kerola <kerolasa@iki.fi>
2024-03-26 18:02:49 +0000

    check spf records contain sensible data
    If nothing else this is a nice typo detector.
    
    Signed-off-by: Sami Kerola <kerolasa@iki.fi>

--- libdctlint/record.go
+++ libdctlint/record.go
@@ -2,6 +2,7 @@ package libdctlint
 
 import (
 	"reflect"
+	"regexp"
 	"strings"
 
 	"github.com/Domain-Connect/dc-template-linter/exitvals"
@@ -21,14 +22,13 @@ func (conf *Conf) checkRecord(
 ) exitvals.CheckSeverity {
 	// A record specific init
 	exitVal := exitvals.CheckOK
-	rlog := conf.tlog.With().Str("groupid", record.GroupID).Int("record", rnum).Logger()
-	rlog.Debug().Str("type", record.Type).Str("host", record.Host).Msg("check record")
+	rlog := conf.tlog.With().Str("groupid", record.GroupID).Int("record", rnum).Str("type", record.Type).Logger()
+	rlog.Debug().Str("host", record.Host).Msg("check record")
 
 	// Try to catch CNAME usage with other records
 	if t, ok := conflictingTypes[record.GroupID+"/"+record.Host]; ok && (t == strCNAME || record.Type == strCNAME) {
 		rlog.Error().
 			Str("host", record.Host).
-			Str("type", record.Type).
 			Str("othertype", t).
 			Msg("CNAME cannot be mixed with other record types")
 		exitVal |= exitvals.CheckError
@@ -40,24 +40,24 @@ func (conf *Conf) checkRecord(
 	case strCNAME, "NS":
 		if record.Host == "@" {
 			if conf.cloudflare {
-				rlog.Info().Str("type", record.Type).Msg("domains must use Cloudflares CNAME flattening setting")
+				rlog.Info().Msg("domains must use Cloudflares CNAME flattening setting")
 				exitVal |= exitvals.CheckInfo
 			} else if !template.HostRequired {
-				rlog.Error().Str("type", record.Type).Msg("record host must not be @ when template hostRequired is false")
+				rlog.Error().Msg("record host must not be @ when template hostRequired is false")
 				exitVal |= exitvals.CheckError
 			}
 		}
 		fallthrough
 	case "A", "AAAA":
 		if record.Host == "" {
-			rlog.Error().Str("type", record.Type).Msg("record host must not be empty")
+			rlog.Error().Msg("record host must not be empty")
 			exitVal |= exitvals.CheckError
 		}
 		exitVal |= targetCheck(record, "pointsTo", rlog)
 
 	case "TXT":
 		if record.Host == "" {
-			rlog.Error().Str("type", record.Type).Msg("record host must not be empty")
+			rlog.Error().Msg("record host must not be empty")
 			exitVal |= exitvals.CheckError
 		}
 		exitVal |= targetCheck(record, "data", rlog)
@@ -71,61 +71,50 @@ func (conf *Conf) checkRecord(
 				exitVal |= exitvals.CheckInfo
 			}
 		} else if record.TxtCMM == "Prefix" && record.TxtCMP == "" {
-			rlog.Warn().Str("type", record.Type).Msg("record txtConflictMatchingPrefix is not defined")
+			rlog.Warn().Msg("record txtConflictMatchingPrefix is not defined")
 			exitVal |= exitvals.CheckWarn
 		}
 
 	case "MX":
 		if record.Host == "" {
-			rlog.Error().Str("type", record.Type).Msg("record host must not be empty")
+			rlog.Error().Msg("record host must not be empty")
 			exitVal |= exitvals.CheckError
 		}
 		exitVal |= targetCheck(record, "pointsTo", rlog)
 		if record.Priority < 0 || max31b < record.Priority {
-			rlog.Error().Str("type", record.Type).Int("priority", int(record.Priority)).Msg("invalid priority")
+			rlog.Error().Int("priority", int(record.Priority)).Msg("invalid priority")
 			exitVal |= exitvals.CheckError
 		}
 
 	case "SRV":
 		exitVal |= targetCheck(record, "target", rlog)
 		if isInvalidProtocol(record.Protocol) {
-			rlog.Warn().Str("type", record.Type).Str("protocol", record.Protocol).Msg("invalid protocol")
+			rlog.Warn().Str("protocol", record.Protocol).Msg("invalid protocol")
 			exitVal |= exitvals.CheckWarn
 		}
 		if record.Priority < 0 || max31b < record.Priority {
-			rlog.Error().Str("type", record.Type).Int("priority", int(record.Priority)).Msg("invalid priority")
+			rlog.Error().Int("priority", int(record.Priority)).Msg("invalid priority")
 			exitVal |= exitvals.CheckError
 		}
 		if record.Service == "" {
-			rlog.Error().Str("type", record.Type).Msg("record service must not be empty")
+			rlog.Error().Msg("record service must not be empty")
 			exitVal |= exitvals.CheckError
 		}
 		if record.Weight < 0 || max31b < record.Weight {
-			rlog.Error().Str("type", record.Type).Int("weight", int(record.Weight)).Msg("invalid weight")
+			rlog.Error().Int("weight", int(record.Weight)).Msg("invalid weight")
 			exitVal |= exitvals.CheckError
 		}
 		if record.Port < 1 || max16b < record.Port {
-			rlog.Error().Str("type", record.Type).Int("port", int(record.Port)).Msg("invalid port")
+			rlog.Error().Int("port", int(record.Port)).Msg("invalid port")
 			exitVal |= exitvals.CheckError
 		}
 
 	case "SPFM":
 		if record.Host == "" {
-			rlog.Error().Str("type", record.Type).Msg("record host must not be empty")
+			rlog.Error().Msg("record host must not be empty")
 			exitVal |= exitvals.CheckError
 		}
-		if record.SPFRules == "" {
-			rlog.Error().Str("type", record.Type).Msg("record spfRules must not be empty")
-			exitVal |= exitvals.CheckError
-		}
-		if strings.HasPrefix(record.SPFRules, "v=spf1") {
-			rlog.Error().Str("type", record.Type).Msg("spfRules must not include v=spf1")
-			exitVal |= exitvals.CheckError
-		}
-		if strings.HasSuffix(record.SPFRules, "all") {
-			rlog.Error().Str("type", record.Type).Msg("spfRules must not include trailing all rule")
-			exitVal |= exitvals.CheckError
-		}
+		exitVal |= checkSPFRules(strings.ToLower(record.SPFRules), rlog)
 
 	case "APEXCNAME":
 		if conf.cloudflare {
@@ -135,11 +124,11 @@ func (conf *Conf) checkRecord(
 
 	case "REDIR301", "REDIR302":
 		if record.Target == "" {
-			rlog.Error().Str("type", record.Type).Msg("record target must not be empty")
+			rlog.Error().Msg("record target must not be empty")
 			exitVal |= exitvals.CheckError
 		}
 	default:
-		rlog.Info().Str("type", record.Type).Msg("unusual record type check DNS providers if they support it")
+		rlog.Info().Msg("unusual record type check DNS providers if they support it")
 		exitVal |= exitvals.CheckInfo
 	}
 
@@ -160,10 +149,10 @@ func (conf *Conf) checkRecord(
 
 	// A calid json int can be out of bounds in DNS
 	if record.TTL < 0 || max31b < record.TTL {
-		rlog.Error().Str("type", record.Type).Int("ttl", int(record.TTL)).Msg("invalid TTL")
+		rlog.Error().Int("ttl", int(record.TTL)).Msg("invalid TTL")
 		exitVal |= exitvals.CheckError
 	} else if conf.cloudflare && record.TTL == 0 {
-		rlog.Info().Str("type", record.Type).Int("ttl", 0).Msg("Cloudflare will replace zero ttl with value of 300")
+		rlog.Info().Int("ttl", 0).Msg("Cloudflare will replace zero ttl with value of 300")
 		exitVal |= exitvals.CheckInfo
 	}
 
@@ -218,13 +207,13 @@ func targetCheck(record internal.Record, requiredField string, rlog zerolog.Logg
 		if ok {
 			csv := strings.Split(jsonTag, ",")
 			if csv[0] == "" {
-				rlog.Error().Str("type", record.Type).Msg("json tag not defined")
+				rlog.Error().Msg("json tag not defined")
 				exitVal |= exitvals.CheckError
 				continue
 			}
 			if csv[0] == requiredField {
 				if reflect.ValueOf(record).FieldByName(field.Name).String() == "" {
-					rlog.Error().Str("field", requiredField).Str("type", record.Type).Msg("required field is missing")
+					rlog.Error().Str("field", requiredField).Msg("required field is missing")
 					exitVal |= exitvals.CheckError
 				}
 				continue
@@ -234,10 +223,92 @@ func targetCheck(record internal.Record, requiredField string, rlog zerolog.Logg
 			}
 			if slices.Contains(mutuallyExclusive, csv[0]) {
 				if reflect.ValueOf(record).FieldByName(field.Name).String() != "" {
-					rlog.Info().Str("field", csv[0]).Str("type", record.Type).Msg("unnecessary field found")
+					rlog.Info().Str("field", csv[0]).Msg("unnecessary field found")
 					exitVal |= exitvals.CheckInfo
 				}
 			}
+		}
+	}
+
+	return exitVal
+}
+
+type spfTrack struct {
+	exp      bool
+	redirect bool
+}
+
+var modifierRe = regexp.MustCompile(`^((?i)[a-z][a-z0-9_.-]*)=(.*)`)
+
+func checkSPFRules(rules string, rlog zerolog.Logger) exitvals.CheckSeverity {
+	exitVal := exitvals.CheckOK
+
+	if rules == "" {
+		rlog.Error().Msg("record spfRules must not be empty")
+		return exitvals.CheckError
+	}
+	if strings.HasPrefix(rules, "v=spf1") {
+		rlog.Error().Msg("spfRules must not include v=spf1")
+		exitVal |= exitvals.CheckError
+	}
+	if strings.HasSuffix(rules, "all") {
+		rlog.Error().Msg("spfRules must not include trailing all rule")
+		exitVal |= exitvals.CheckError
+	}
+
+	fields := strings.Fields(rules)
+	track := spfTrack{}
+
+	for _, field := range fields {
+		if isVariable(field) {
+			// variables are ok
+			continue
+		}
+
+		matches := modifierRe.FindStringSubmatch(field)
+		if matches != nil {
+			switch matches[1] {
+			case "redirect":
+				if track.redirect {
+					rlog.Error().Msg("spfRules has multiple redirect fields")
+					exitVal |= exitvals.CheckError
+				}
+				track.redirect = true
+			case "exp":
+				if track.exp {
+					rlog.Error().Msg("spfRules has multiple exp fields")
+					exitVal |= exitvals.CheckError
+				}
+				track.exp = true
+			default:
+				rlog.Error().Str("field", matches[1]).Msg("spfRules contains unknown macro field")
+				exitVal |= exitvals.CheckError
+			}
+			continue
+		}
+
+		switch field[0] {
+		case '+':
+			field = field[1:]
+		case '-':
+			field = field[1:]
+		case '~':
+			field = field[1:]
+		case '?':
+			field = field[1:]
+		}
+
+		separator := strings.IndexAny(field, ":")
+		if -1 < separator {
+			field = field[:separator]
+		}
+
+		switch field {
+		case "include", "a", "mx", "ptr", "ip4", "ip6", "exists":
+			// these are ok
+		default:
+			rlog.Error().Str("modifier", field).Msg("spfRules contains unknown modifier")
+			exitVal |= exitvals.CheckError
 		}
 	}
 
--- version.go
+++ version.go
@@ -1,3 +1,3 @@
 package main
 
-const dcTemplateLinterVersion uint = 46
+const dcTemplateLinterVersion uint = 47
