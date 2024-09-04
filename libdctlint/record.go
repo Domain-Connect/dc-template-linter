Domain-Connect/dc-template-linter

commit f9d323dce08adefb10c789a14ce3dbe1af600915

spec: allow number types to be ints, strings, and/or variables
After this change numbers are always quoted after reformatting.  That is the
only way to be somewhat consistent what is the representation format when
%variables% need to be supported, and they need to be strings.

As a practical example, one could have the following in a template (and it
would fail, but it's obvious why - see the hostname).

    "records": [
        {
            "type": "A",
            "host": "the-ttl-is-plain-number-this-is-ok",
            "ttl": 1800
        },
        {
            "type": "A",
            "host": "the-ttl-is-quoted-number-this-is-ok",
            "ttl": "8100"
        },
        {
            "type": "A",
            "host": "the-ttl-is-variable-this-is-ok",
            "ttl": "%varttl%"
        },
        {
            "type": "A",
            "host": "ttl-is-BROKEN-it-cannot-be-just-a-string",
            "ttl": "just-a-string"
        }
    ]

Reference: https://github.com/Domain-Connect/spec/pull/100
Signed-off-by: Sami Kerola <kerolasa@iki.fi>

libdctlint/record.go
@@ -86,8 +86,8 @@ func (conf *Conf) checkRecord(
 			exitVal |= exitvals.CheckError
 		}
 		exitVal |= targetCheck(record, "pointsTo", rlog)
-		if record.Priority < 0 || max31b < record.Priority {
-			rlog.Error().Int("priority", int(record.Priority)).EmbedObject(internal.DCTL1015).Msg("")
+		if priority, ok := record.Priority.Uint32(); ok && (priority < 0 || max31b < priority) {
+			rlog.Error().Uint32("priority", priority).EmbedObject(internal.DCTL1015).Msg("")
 			exitVal |= exitvals.CheckError
 		}
 
@@ -97,20 +97,20 @@ func (conf *Conf) checkRecord(
 			rlog.Warn().Str("protocol", record.Protocol).EmbedObject(internal.DCTL1015).Msg("")
 			exitVal |= exitvals.CheckWarn
 		}
-		if record.Priority < 0 || max31b < record.Priority {
-			rlog.Error().Int("priority", int(record.Priority)).EmbedObject(internal.DCTL1015).Msg("")
+		if priority, ok := record.Priority.Uint32(); ok && (priority < 0 || max31b < priority) {
+			rlog.Error().Uint32("priority", priority).EmbedObject(internal.DCTL1015).Msg("")
 			exitVal |= exitvals.CheckError
 		}
 		if record.Service == "" {
 			rlog.Error().Str("key", "service").EmbedObject(internal.DCTL1013).Msg("")
 			exitVal |= exitvals.CheckError
 		}
-		if record.Weight < 0 || max31b < record.Weight {
-			rlog.Error().Int("weight", int(record.Weight)).EmbedObject(internal.DCTL1015).Msg("")
+		if weight, ok := record.Priority.Uint32(); ok && (weight < 0 || max31b < weight) {
+			rlog.Error().Uint32("weight", weight).EmbedObject(internal.DCTL1015).Msg("")
 			exitVal |= exitvals.CheckError
 		}
-		if record.Port < 1 || max16b < record.Port {
-			rlog.Error().Int("port", int(record.Port)).EmbedObject(internal.DCTL1015).Msg("")
+		if port, ok := record.Priority.Uint16(); ok && (port < 1 || max16b < port) {
+			rlog.Error().Uint16("port", port).EmbedObject(internal.DCTL1015).Msg("")
 			exitVal |= exitvals.CheckError
 		}
 
@@ -153,18 +153,17 @@ func (conf *Conf) checkRecord(
 	}
 
 	// A calid json int can be out of bounds in DNS
-	invalidTTL := false
-	if record.TTL < 0 || MaxTTL < record.TTL {
-		rlog.Error().Int("ttl", int(record.TTL)).EmbedObject(internal.DCTL1015).Msg("")
+	ttl, ok := record.TTL.Uint32()
+	if ok && ttl < 0 || MaxTTL < ttl {
+		rlog.Error().Uint32("ttl", ttl).EmbedObject(internal.DCTL1015).Msg("")
 		exitVal |= exitvals.CheckError
-		invalidTTL = true
-	} else if conf.cloudflare && record.TTL == 0 {
+	} else if ok && conf.cloudflare && ttl == 0 {
 		rlog.Info().EmbedObject(internal.DCTL5010).Msg("")
 		exitVal |= exitvals.CheckInfo
 	}
-	if invalidTTL || record.TTL == 0 && conf.inplace && 0 < conf.ttl && requiresTTL(record.Type) {
-		rlog.Info().Uint("ttl", conf.ttl).Msg("adding ttl to the record")
-		record.TTL = internal.SINT(conf.ttl)
+	if !ok && ttl == 0 && conf.inplace && 0 < conf.ttl && requiresTTL(record.Type) && isVariable(string(record.TTL)) {
+		rlog.Info().Uint32("ttl", conf.ttl).Msg("adding ttl to the record")
+		record.TTL.SetUint32(conf.ttl)
 	}
 
 	// Enforce Domain Connect spec

version.go
@@ -1,3 +1,3 @@
 package main
 
-const dcTemplateLinterVersion uint = 57
+const dcTemplateLinterVersion uint = 58

internal/dctl.go
@@ -78,7 +78,7 @@ var dctlToString = map[DCTL]string{
 
 	// domain connect specific messages
 	DCTL1000: "ttl value exceeds maximum",
-	DCTL1001: "do not quote an integer",
+	DCTL1001: "<not in use after string-interger-variable values>",
 	DCTL1002: "id contains invalid characters",
 	DCTL1003: "file name does not use required pattern",
 	DCTL1004: "duplicate provierId + serviceId detected",

internal/json.go
@@ -2,13 +2,8 @@ package internal
 
 import (
 	"encoding/json"
-	"errors"
+	"fmt"
 	"strconv"
-	"sync"
-
-	"github.com/Domain-Connect/dc-template-linter/exitvals"
-
-	"github.com/rs/zerolog"
 )
 
 type Template struct {
@@ -54,63 +49,61 @@ type Record struct {
 	SPFRules  string `json:"spfRules,omitempty" validate:"min=1"`
 }
 
-type SINT int
-
-// exitVal is a side channel to catch UnmarshalJSON failure. The standard
-// golang json parser does not have means for extra values, and sending a
-// message via error is not great either - it will stop parsing, which is
-// annoying if early entry in records list is stringy entry but can be
-// recovered while causing warning.
-var exitVal exitvals.CheckSeverity
-
-// smuggledLog is similar side channel to exitVal but for json parser extra
-// input rather than output.
-var smuggledLog zerolog.Logger
-
-// lock will ensure only one exitVal / logger pair is in use.
-var mu sync.Mutex
+type SINT string
 
 // UnmarshalJSON can take string and integer version of an int and return
 // int. When int is integer all happens without complaints, but a string int
 // will cause warning and conversion to int. The json parsing will fail
 // completely when string is not integer at all.
 func (sint *SINT) UnmarshalJSON(b []byte) error {
-	locked := mu.TryLock()
-	if locked {
-		return errors.New("BUG: SetLogger() call was not used")
-	}
-	exitVal = exitvals.CheckOK
-
-	if b[0] != '"' {
-		err := json.Unmarshal(b, (*int)(sint))
-		if err != nil {
-			exitVal = exitvals.CheckError
-		}
-		return err
-	}
-	var s string
-	err := json.Unmarshal(b, &s)
+	s, err := strconv.Unquote(string(b))
 	if err != nil {
-		exitVal = exitvals.CheckError
-		return err
+		// cannot unquote, value is numeric
+		*sint = SINT(b)
+		return nil
 	}
-	i, err := strconv.Atoi(s)
-	if err != nil {
-		exitVal = exitvals.CheckError
-		return err
-	}
-	exitVal = exitvals.CheckInfo
-	smuggledLog.Info().Str("value", s).EmbedObject(DCTL1001).Msg("")
-	*sint = SINT(i)
+	*sint = SINT(s)
 	return nil
 }
 
-func SetLogger(l zerolog.Logger) {
-	mu.Lock()
-	smuggledLog = l
+func (sint *SINT) MarshalJSON() ([]byte, error) {
+	s := string(*sint)
+	c := s[0]
+	if (c < '0' || '9' < c) && (c != '%' || s[len(s)-1] != '%') {
+		return []byte{}, fmt.Errorf("invalid string-interger-variable: %s", string(*sint))
+	}
+	quoted := fmt.Sprintf("\"%s\"", *sint)
+	return []byte(quoted), nil
 }
 
-func GetUnmarshalStatus() exitvals.CheckSeverity {
-	defer mu.Unlock()
-	return exitVal
+func (sint *SINT) Inc() {
+	i, err := strconv.Atoi(string(*sint))
+	if err != nil {
+		return
+	}
+	i++
+	s := fmt.Sprintf("%d", i)
+	*sint = SINT(s)
+}
+
+func (sint *SINT) Uint32() (uint32, bool) {
+	var i uint32
+	err := json.Unmarshal([]byte(*sint), &i)
+	if err != nil {
+		return i, false
+	}
+	return i, true
+}
+
+func (sint *SINT) SetUint32(value uint32) {
+	s := fmt.Sprintf("%d", value)
+	*sint = SINT(s)
+}
+
+func (sint *SINT) Uint16() (uint16, bool) {
+	i, err := strconv.Atoi(string(*sint))
+	if err != nil {
+		return 0, false
+	}
+	return uint16(i), true
 }

libdctlint/config.go
@@ -16,7 +16,7 @@ type Conf struct {
 	inplace     bool
 	increment   bool
 	prettyPrint bool
-	ttl         uint
+	ttl         uint32
 }
 
 // NewConf will create template check configuration.
@@ -61,7 +61,7 @@ func (c *Conf) SetPrettyPrint(b bool) *Conf {
 	return c
 }
 
-func (c *Conf) SetTTL(t uint) *Conf {
+func (c *Conf) SetTTL(t uint32) *Conf {
 	c.ttl = t
 	return c
 }

libdctlint/lib.go
@@ -32,7 +32,6 @@ const (
 // outside of this project.
 func (conf *Conf) GetAndCheckTemplate(f *bufio.Reader) (internal.Template, exitvals.CheckSeverity) {
 	conf.SetLogger(log.With().Str("template", conf.fileName).Logger())
-	internal.SetLogger(conf.tlog)
 	conf.tlog.Debug().Msg("starting template check")
 
 	// Decode json
@@ -40,12 +39,11 @@ func (conf *Conf) GetAndCheckTemplate(f *bufio.Reader) (internal.Template, exitv
 	decoder.DisallowUnknownFields()
 	var template internal.Template
 	err := decoder.Decode(&template)
-	exitVal := internal.GetUnmarshalStatus()
 	if err != nil {
-		conf.tlog.Error().Err(err).EmbedObject(internal.DCTL0003).Msg("")
+		conf.tlog.Error().Err(err).EmbedObject(internal.DCTL0003).Msg("a")
 		return template, exitvals.CheckFatal
 	}
-	exitVal |= conf.checkTemplate(template)
+	exitVal := conf.checkTemplate(template)
 	return template, exitVal
 }
 
@@ -101,7 +99,7 @@ func (conf *Conf) checkTemplate(template internal.Template) exitvals.CheckSeveri
 	}
 
 	// Field checks provided by this file
-	if template.Version < 0 {
+	if len(template.Version) == 0 {
 		conf.tlog.Info().EmbedObject(internal.DCTL1006).Msg("")
 		exitVal |= exitvals.CheckInfo
 	}
@@ -151,12 +149,12 @@ func (conf *Conf) checkTemplate(template internal.Template) exitvals.CheckSeveri
 	// Pretty printing and/or inplace write output
 	if conf.prettyPrint || conf.inplace {
 		if conf.increment {
-			template.Version++
+			template.Version.Inc()
 		}
 		// Convert to json
 		marshaled, err := json.Marshal(template)
 		if err != nil {
-			conf.tlog.Error().Err(err).EmbedObject(internal.DCTL0003).Msg("")
+			conf.tlog.Error().Err(err).EmbedObject(internal.DCTL0003).Msg("b")
 			return exitVal | exitvals.CheckError
 		}
 
@@ -164,7 +162,7 @@ func (conf *Conf) checkTemplate(template internal.Template) exitvals.CheckSeveri
 		var out bytes.Buffer
 		err = json.Indent(&out, marshaled, "", "    ")
 		if err != nil {
-			conf.tlog.Error().Err(err).EmbedObject(internal.DCTL0003).Msg("")
+			conf.tlog.Error().Err(err).EmbedObject(internal.DCTL0003).Msg("c")
 			return exitVal | exitvals.CheckError
 		}
 		_, err = fmt.Fprintf(&out, "\n")

main.go
@@ -44,7 +44,7 @@ func main() {
 		flag.PrintDefaults()
 		_, _ = fmt.Fprintf(os.Stderr, "Warning. -inplace and -pretty will remove zero priority MX and SRV fields\n")
 		_, _ = fmt.Fprintf(os.Stderr, "You can find long DCTL explanations in wiki\n")
-		_, _ = fmt.Fprintf(os.Stderr, "e.g., https://github.com/Domain-Connect/dc-template-linter/wiki/DCTL1001\n")
+		_, _ = fmt.Fprintf(os.Stderr, "e.g., https://github.com/Domain-Connect/dc-template-linter/wiki/DCTL1003\n")
 	}
 	checkLogos := flag.Bool("logos", false, "check logo urls are reachable (requires network)")
 	cloudflare := flag.Bool("cloudflare", false, "use Cloudflare specific template rules")
@@ -83,7 +83,7 @@ func main() {
 		exitVal = conf.CheckTemplate(reader)
 	} else {
 		conf.SetInplace(*inplace).
-			SetTTL(*ttl).
+			SetTTL(uint32(*ttl)).
 			SetIncrement(*increment)
 
 		for _, arg := range flag.Args() {

commit 2ff5dfc8696931afc6cf5e80d32c018e344b135b

spec: fix copy-paste error
The commit that changed integers to string-variable-numbers accidentally
used wrong field in couple SRV record checks.

Fixes: f9d323dce08adefb10c789a14ce3dbe1af600915
Signed-off-by: Sami Kerola <kerolasa@iki.fi>

libdctlint/record.go
@@ -105,11 +105,11 @@ func (conf *Conf) checkRecord(
 			rlog.Error().Str("key", "service").EmbedObject(internal.DCTL1013).Msg("")
 			exitVal |= exitvals.CheckError
 		}
-		if weight, ok := record.Priority.Uint32(); ok && (weight < 0 || max31b < weight) {
+		if weight, ok := record.Weight.Uint32(); ok && (weight < 0 || max31b < weight) {
 			rlog.Error().Uint32("weight", weight).EmbedObject(internal.DCTL1015).Msg("")
 			exitVal |= exitvals.CheckError
 		}
-		if port, ok := record.Priority.Uint16(); ok && (port < 1 || max16b < port) {
+		if port, ok := record.Port.Uint16(); ok && (port < 1 || max16b < port) {
 			rlog.Error().Uint16("port", port).EmbedObject(internal.DCTL1015).Msg("")
 			exitVal |= exitvals.CheckError
 		}

version.go
@@ -1,3 +1,3 @@
 package main
 
-const dcTemplateLinterVersion uint = 62
+const dcTemplateLinterVersion uint = 63