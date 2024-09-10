Domain-Connect/dc-template-linter

commit 46fb8d23fa589a4a99af7f7dcb07c75deea56e09

DCTL1022: check SyncPubKeyDomain and SyncRedirectDomain hostnames
Templates need to contain valid hostnames in the Sync.. fields.

Signed-off-by: Sami Kerola <kerolasa@iki.fi>

libdctlint/lib.go
@@ -21,6 +21,7 @@ import (
 
 	"github.com/go-playground/validator/v10"
 	"github.com/rs/zerolog/log"
+	"golang.org/x/net/idna"
 )
 
 const (
@@ -130,6 +131,16 @@ func (conf *Conf) checkTemplate(template internal.Template) exitvals.CheckSeveri
 	// Logo url reachability check
 	if err := conf.isUnreachable(template.Logo); err != nil {
 		conf.tlog.Warn().Err(err).Str("logoUrl", template.Logo).EmbedObject(internal.DCTL1010).Msg("")
+		exitVal |= exitvals.CheckWarn
+	}
+
+	if err := checkHostname(template.SyncPubKeyDomain); err != nil {
+		conf.tlog.Error().Err(err).Str("SyncPubKeyDomain", template.SyncPubKeyDomain).EmbedObject(internal.DCTL1022).Msg("")
+		exitVal |= exitvals.CheckWarn
+	}
+
+	if err := checkSyncRedirectDomain(template.SyncRedirectDomain); err != nil {
+		conf.tlog.Error().Err(err).Str("SyncRedirectDomain", template.SyncRedirectDomain).EmbedObject(internal.DCTL1022).Msg("")
 		exitVal |= exitvals.CheckWarn
 	}
 
@@ -247,6 +258,26 @@ func (conf *Conf) writeBack(out bytes.Buffer) exitvals.CheckSeverity {
 
 func isVariable(s string) bool {
 	return strings.Count(s, "%") > 1
+}
+
+func checkSyncRedirectDomain(srd string) (err error) {
+	srdList := strings.Split(srd, ",")
+	for i := range srdList {
+		trimmed := strings.TrimSpace(srdList[i])
+		e := checkHostname(trimmed)
+		if e != nil {
+			err = e
+		}
+	}
+	return err
+}
+
+func checkHostname(host string) error {
+	parser := idna.New(
+		idna.MapForLookup(),
+	)
+	_, err := parser.ToASCII(host)
+	return err
 }
 
 func (conf *Conf) cloudflareTemplateChecks(template internal.Template) exitvals.CheckSeverity {

version.go
@@ -1,3 +1,3 @@
 package main
 
-const dcTemplateLinterVersion uint = 61
+const dcTemplateLinterVersion uint = 62

internal/dctl.go
@@ -48,6 +48,7 @@ const (
 	DCTL1019 DCTL = 1019
 	DCTL1020 DCTL = 1020
 	DCTL1021 DCTL = 1021
+	DCTL1022 DCTL = 1022
 
 	DCTL5000 DCTL = 5000
 	DCTL5001 DCTL = 5001
@@ -99,6 +100,7 @@ var dctlToString = map[DCTL]string{
 	DCTL1019: "variable contains invalid character",
 	DCTL1020: "variable is not terminated",
 	DCTL1021: "missing from iana definitions",
+	DCTL1022: "invalid hostname",
 
 	// cloudflare messages
 	DCTL5000: "syncBlock is not supported",

commit 365b2b25fa633a9cf9a5d70a7026a3ae2d949615

DCTL1022: improve SyncPubKeyDomain and SyncRedirectDomain checks
Earlier check did not catch domains that were just hostnames.  And while
changing the library lets use one that will check a user hostname is
included in Public Suffix List.  This allows noticing domains using tld that
are not known by root servers.

Fixes: 46fb8d23fa589a4a99af7f7dcb07c75deea56e09
Signed-off-by: Sami Kerola <kerolasa@iki.fi>

go.mod
@@ -1,22 +1,24 @@
 module github.com/Domain-Connect/dc-template-linter
 
-go 1.21.0
+go 1.22.0
 
 require (
-	github.com/go-playground/validator/v10 v10.22.0
+	github.com/THREATINT/go-net v1.2.38
+	github.com/go-playground/validator/v10 v10.22.1
 	github.com/mattn/go-isatty v0.0.20
 	github.com/rs/zerolog v1.33.0
-	golang.org/x/exp v0.0.0-20240707233637-46b078467d37
+	golang.org/x/exp v0.0.0-20240909161429-701f63a606c0
 )
 
 require (
-	github.com/gabriel-vasile/mimetype v1.4.4 // indirect
+	github.com/PuerkitoBio/purell v1.2.1 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.5 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
-	golang.org/x/crypto v0.25.0 // indirect
-	golang.org/x/net v0.27.0 // indirect
-	golang.org/x/sys v0.22.0 // indirect
-	golang.org/x/text v0.16.0 // indirect
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
 )

go.sum
@@ -1,16 +1,20 @@
+github.com/PuerkitoBio/purell v1.2.1 h1:QsZ4TjvwiMpat6gBCBxEQI0rcS9ehtkKtSpiUnd9N28=
+github.com/PuerkitoBio/purell v1.2.1/go.mod h1:ZwHcC/82TOaovDi//J/804umJFFmbOHPngi8iYYv/Eo=
+github.com/THREATINT/go-net v1.2.38 h1:ZhiP0Rr8ZtOURlUh9YyAYklLDO+gQaUSLooqhsu6Qbo=
+github.com/THREATINT/go-net v1.2.38/go.mod h1:kpRfrwLUkXxuqPo5Ea0B6qkLmHBPSxVDtq1gc9ZyqvU=
 github.com/coreos/go-systemd/v22 v22.5.0/go.mod h1:Y58oyj3AT4RCenI/lSvhwexgC+NSVTIJ3seZv2GcEnc=
 github.com/davecgh/go-spew v1.1.1 h1:vj9j/u1bqnvCEfJOwUhtlOARqs3+rkHYY13jYWTU97c=
 github.com/davecgh/go-spew v1.1.1/go.mod h1:J7Y8YcW2NihsgmVo/mv3lAwl/skON4iLHjSsI+c5H38=
-github.com/gabriel-vasile/mimetype v1.4.4 h1:QjV6pZ7/XZ7ryI2KuyeEDE8wnh7fHP9YnQy+R0LnH8I=
-github.com/gabriel-vasile/mimetype v1.4.4/go.mod h1:JwLei5XPtWdGiMFB5Pjle1oEeoSeEuJfJE+TtfvdB/s=
+github.com/gabriel-vasile/mimetype v1.4.5 h1:J7wGKdGu33ocBOhGy0z653k/lFKLFDPJMG8Gql0kxn4=
+github.com/gabriel-vasile/mimetype v1.4.5/go.mod h1:ibHel+/kbxn9x2407k1izTA1S81ku1z/DlgOW2QE0M4=
 github.com/go-playground/assert/v2 v2.2.0 h1:JvknZsQTYeFEAhQwI4qEt9cyV5ONwRHC+lYKSsYSR8s=
 github.com/go-playground/assert/v2 v2.2.0/go.mod h1:VDjEfimB/XKnb+ZQfWdccd7VUvScMdVu0Titje2rxJ4=
 github.com/go-playground/locales v0.14.1 h1:EWaQ/wswjilfKLTECiXz7Rh+3BjFhfDFKv/oXslEjJA=
 github.com/go-playground/locales v0.14.1/go.mod h1:hxrqLVvrK65+Rwrd5Fc6F2O76J/NuW9t0sjnWqG1slY=
 github.com/go-playground/universal-translator v0.18.1 h1:Bcnm0ZwsGyWbCzImXv+pAJnYK9S473LQFuzCbDbfSFY=
 github.com/go-playground/universal-translator v0.18.1/go.mod h1:xekY+UJKNuX9WP91TpwSH2VMlDf28Uj24BCp08ZFTUY=
-github.com/go-playground/validator/v10 v10.22.0 h1:k6HsTZ0sTnROkhS//R0O+55JgM8C4Bx7ia+JlgcnOao=
-github.com/go-playground/validator/v10 v10.22.0/go.mod h1:dbuPbCMFw/DrkbEynArYaCwl3amGuJotoKCe95atGMM=
+github.com/go-playground/validator/v10 v10.22.1 h1:40JcKH+bBNGFczGuoBYgX4I6m/i27HYW8P9FDk5PbgA=
+github.com/go-playground/validator/v10 v10.22.1/go.mod h1:dbuPbCMFw/DrkbEynArYaCwl3amGuJotoKCe95atGMM=
 github.com/godbus/dbus/v5 v5.0.4/go.mod h1:xhWf0FNVPg57R7Z0UbKHbJfkEywrmjJnf7w5xrFpKfA=
 github.com/leodido/go-urn v1.4.0 h1:WT9HwE9SGECu3lg4d/dIA+jxlljEa1/ffXKmRjqdmIQ=
 github.com/leodido/go-urn v1.4.0/go.mod h1:bvxc+MVxLKB4z00jd1z+Dvzr47oO32F/QSNjSBOlFxI=
@@ -28,18 +32,18 @@ github.com/rs/zerolog v1.33.0 h1:1cU2KZkvPxNyfgEmhHAz/1A9Bz+llsdYzklWFzgp0r8=
 github.com/rs/zerolog v1.33.0/go.mod h1:/7mN4D5sKwJLZQ2b/znpjC3/GQWY/xaDXUM0kKWRHss=
 github.com/stretchr/testify v1.8.4 h1:CcVxjf3Q8PM0mHUKJCdn+eZZtm5yQwehR5yeSVQQcUk=
 github.com/stretchr/testify v1.8.4/go.mod h1:sz/lmYIOXD/1dqDmKjjqLyZ2RngseejIcXlSw2iwfAo=
-golang.org/x/crypto v0.25.0 h1:ypSNr+bnYL2YhwoMt2zPxHFmbAN1KZs/njMG3hxUp30=
-golang.org/x/crypto v0.25.0/go.mod h1:T+wALwcMOSE0kXgUAnPAHqTLW+XHgcELELW8VaDgm/M=
-golang.org/x/exp v0.0.0-20240707233637-46b078467d37 h1:uLDX+AfeFCct3a2C7uIWBKMJIR3CJMhcgfrUAqjRK6w=
-golang.org/x/exp v0.0.0-20240707233637-46b078467d37/go.mod h1:M4RDyNAINzryxdtnbRXRL/OHtkFuWGRjvuhBJpk2IlY=
-golang.org/x/net v0.27.0 h1:5K3Njcw06/l2y9vpGCSdcxWOYHOUk3dVNGDXN+FvAys=
-golang.org/x/net v0.27.0/go.mod h1:dDi0PyhWNoiUOrAS8uXv/vnScO4wnHQO4mj9fn/RytE=
+golang.org/x/crypto v0.27.0 h1:GXm2NjJrPaiv/h1tb2UH8QfgC/hOf/+z0p6PT8o1w7A=
+golang.org/x/crypto v0.27.0/go.mod h1:1Xngt8kV6Dvbssa53Ziq6Eqn0HqbZi5Z6R0ZpwQzt70=
+golang.org/x/exp v0.0.0-20240909161429-701f63a606c0 h1:e66Fs6Z+fZTbFBAxKfP3PALWBtpfqks2bwGcexMxgtk=
+golang.org/x/exp v0.0.0-20240909161429-701f63a606c0/go.mod h1:2TbTHSBQa924w8M6Xs1QcRcFwyucIwBGpK1p2f1YFFY=
+golang.org/x/net v0.29.0 h1:5ORfpBpCs4HzDYoodCDBbwHzdR5UrLBZ3sOnUJmFoHo=
+golang.org/x/net v0.29.0/go.mod h1:gLkgy8jTGERgjzMic6DS9+SP0ajcu6Xu3Orq/SpETg0=
 golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab/go.mod h1:oPkhp1MJrh7nUepCBck5+mAzfO9JrbApNNgaTdGDITg=
 golang.org/x/sys v0.6.0/go.mod h1:oPkhp1MJrh7nUepCBck5+mAzfO9JrbApNNgaTdGDITg=
 golang.org/x/sys v0.12.0/go.mod h1:oPkhp1MJrh7nUepCBck5+mAzfO9JrbApNNgaTdGDITg=
-golang.org/x/sys v0.22.0 h1:RI27ohtqKCnwULzJLqkv897zojh5/DwS/ENaMzUOaWI=
-golang.org/x/sys v0.22.0/go.mod h1:/VUhepiaJMQUp4+oa/7Zr1D23ma6VTLIYjOOTFZPUcA=
-golang.org/x/text v0.16.0 h1:a94ExnEXNtEwYLGJSIUxnWoxoRz/ZcCsV63ROupILh4=
-golang.org/x/text v0.16.0/go.mod h1:GhwF1Be+LQoKShO3cGOHzqOgRrGaYc9AvblQOmPVHnI=
+golang.org/x/sys v0.25.0 h1:r+8e+loiHxRqhXVl6ML1nO3l1+oFoWbnlu2Ehimmi34=
+golang.org/x/sys v0.25.0/go.mod h1:/VUhepiaJMQUp4+oa/7Zr1D23ma6VTLIYjOOTFZPUcA=
+golang.org/x/text v0.18.0 h1:XvMDiNzPAl0jr17s6W9lcaIhGUfUORdGCNsuLmPG224=
+golang.org/x/text v0.18.0/go.mod h1:BuEKDfySbSR4drPmRPG/7iBdf8hvFMuRexcpahXilzY=
 gopkg.in/yaml.v3 v3.0.1 h1:fxVm/GzAzEWqLHuvctI91KS9hhNmmWOoWu0XTYJS7CA=
 gopkg.in/yaml.v3 v3.0.1/go.mod h1:K4uyk7z7BCEPqu6E+C64Yfv1cQ7kz7rIZviUmN+EgEM=

libdctlint/lib.go
@@ -19,9 +19,9 @@ import (
 	"github.com/Domain-Connect/dc-template-linter/exitvals"
 	"github.com/Domain-Connect/dc-template-linter/internal"
 
+	gonet "github.com/THREATINT/go-net"
 	"github.com/go-playground/validator/v10"
 	"github.com/rs/zerolog/log"
-	"golang.org/x/net/idna"
 )
 
 const (
@@ -134,7 +134,7 @@ func (conf *Conf) checkTemplate(template internal.Template) exitvals.CheckSeveri
 		exitVal |= exitvals.CheckWarn
 	}
 
-	if err := checkHostname(template.SyncPubKeyDomain); err != nil {
+	if err := checkFQDN(template.SyncPubKeyDomain); err != nil {
 		conf.tlog.Error().Err(err).Str("SyncPubKeyDomain", template.SyncPubKeyDomain).EmbedObject(internal.DCTL1022).Msg("")
 		exitVal |= exitvals.CheckWarn
 	}
@@ -264,20 +264,28 @@ func checkSyncRedirectDomain(srd string) (err error) {
 	srdList := strings.Split(srd, ",")
 	for i := range srdList {
 		trimmed := strings.TrimSpace(srdList[i])
-		e := checkHostname(trimmed)
-		if e != nil {
+		e := checkFQDN(trimmed)
+		if e != nil && err == nil {
 			err = e
 		}
 	}
 	return err
 }
 
-func checkHostname(host string) error {
-	parser := idna.New(
-		idna.MapForLookup(),
-	)
-	_, err := parser.ToASCII(host)
-	return err
+func checkFQDN(fqdn string) error {
+	if fqdn == "" {
+		return nil
+	}
+	if gonet.IsFQDN(fqdn) {
+		fqdn = gonet.DomainFromFqdn(fqdn)
+	}
+	if !gonet.IsDomain(fqdn) {
+		return fmt.Errorf("failed go-net.IsFQDN() test: '%s'", fqdn)
+	}
+	if strings.IndexRune(fqdn, '.') == -1 {
+		return fmt.Errorf("tld is not allowed")
+	}
+	return nil
 }
 
 func (conf *Conf) cloudflareTemplateChecks(template internal.Template) exitvals.CheckSeverity {

version.go
@@ -1,3 +1,3 @@
 package main
 
-const dcTemplateLinterVersion uint = 64
+const dcTemplateLinterVersion uint = 65