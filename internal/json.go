Domain-Connect/dc-template-linter

commit 1af6b65ef0fc7598fd5a4a33b29802dcd8ef79e4

fix records go struct data validator
Calling validator to parent struct does not cascade to sub-structs.  This
validation oversight has been a bug since from beginning, but luckily none
of the existing templates totally wrong about DCTL1005 errors, there's just
one exception.

The tinkerhost.net.tinkermail.json has used case in sensitive 'essential'
field 'onApply' that is supposed to be 'OnApply'.  That does not comply with
'11.2.  Case Sensitivity' in the draft.

Reference: https://github.com/Domain-Connect/spec/blob/ietf/draft-kowalik-domainconnect-03/draft-kowalik-domainconnect-03.txt#L3888
Signed-off-by: Sami Kerola <kerolasa@iki.fi>

internal/json.go
@@ -32,22 +32,22 @@ type Records []Record
 
 type Record struct {
 	Type      string `json:"type" validate:"required,min=1,max=16"`
-	GroupID   string `json:"groupId,omitempty" validate:"min=1"`
-	Essential string `json:"essential,omitempty" validate:"oneof=Always OnApply"`
-	Host      string `json:"host,omitempty" validate:"min=1,max=255"`
-	Name      string `json:"name,omitempty" validate:"hostname"`
-	PointsTo  string `json:"pointsTo,omitempty" validate:"min=1,max=255"`
-	TTL       SINT   `json:"ttl,omitempty" validate:"required,min=1"`
-	Data      string `json:"data,omitempty" validate:"min=1"`
-	TxtCMM    string `json:"txtConflictMatchingMode,omitempty" validate:"oneof=None All Prefix"`
-	TxtCMP    string `json:"txtConflictMatchingPrefix,omitempty" validate:"min=1"`
-	Priority  SINT   `json:"priority,omitempty"`
-	Weight    SINT   `json:"weight,omitempty"`
-	Port      SINT   `json:"port,omitempty"`
-	Protocol  string `json:"protocol,omitempty" validate:"min=1"`
-	Service   string `json:"service,omitempty" validate:"min=1"`
-	Target    string `json:"target,omitempty" validate:"min=1"`
-	SPFRules  string `json:"spfRules,omitempty" validate:"min=1"`
+	GroupID   string `json:"groupId,omitempty" validate:"omitempty"`
+	Essential string `json:"essential,omitempty" validate:"omitempty,oneof=Always OnApply"`
+	Host      string `json:"host,omitempty" validate:"omitempty,max=255"`
+	Name      string `json:"name,omitempty" validate:"omitempty,max=63"`
+	PointsTo  string `json:"pointsTo,omitempty" validate:"omitempty,max=255"`
+	TTL       SINT   `json:"ttl,omitempty" validate:"omitempty,min=1"`
+	Data      string `json:"data,omitempty" validate:"required_if=Type TXT"`
+	TxtCMM    string `json:"txtConflictMatchingMode,omitempty" validate:"omitempty,oneof=None All Prefix"`
+	TxtCMP    string `json:"txtConflictMatchingPrefix,omitempty" validate:"omitempty"`
+	Priority  SINT   `json:"priority,omitempty validate:"required_if=Type SRV"`
+	Weight    SINT   `json:"weight,omitempty validate:"required_if=Type SRV""`
+	Port      SINT   `json:"port,omitempty validate:"required_if=Type SRV""`
+	Protocol  string `json:"protocol,omitempty" validate:"required_if=Type SRV"`
+	Service   string `json:"service,omitempty" validate:"required_if=Type SRV"`
+	Target    string `json:"target,omitempty" validate:"required_if=Type SRV"`
+	SPFRules  string `json:"spfRules,omitempty" validate:"omitempty,min=1"`
 }
 
 type SINT string

version.go
@@ -1,3 +1,3 @@
 package main
 
-const dcTemplateLinterVersion uint = 85
+const dcTemplateLinterVersion uint = 86

libdctlint/lib.go
@@ -99,6 +99,17 @@ func (conf *Conf) checkTemplate(template internal.Template) exitvals.CheckSeveri
 		}
 	}
 
+	// and validate the records
+	for _, record := range template.Records {
+		err := validate.Struct(record)
+		if err != nil {
+			for _, err := range err.(validator.ValidationErrors) {
+				conf.tlog.Warn().Err(err).EmbedObject(internal.DCTL1005).Msg("")
+				exitVal |= exitvals.CheckWarn
+			}
+		}
+	}
+
 	// Field checks provided by this file
 	if template.Version == 0 {
 		conf.tlog.Info().EmbedObject(internal.DCTL1006).Msg("")

commit 0cb921835904796a1abe3ea4df44f36130bf3b89

fix quotes in struct validate instructions
Fixes: 1af6b65ef0fc7598fd5a4a33b29802dcd8ef79e4
Signed-off-by: Sami Kerola <kerolasa@iki.fi>

internal/json.go
@@ -41,9 +41,9 @@ type Record struct {
 	Data      string `json:"data,omitempty" validate:"required_if=Type TXT"`
 	TxtCMM    string `json:"txtConflictMatchingMode,omitempty" validate:"omitempty,oneof=None All Prefix"`
 	TxtCMP    string `json:"txtConflictMatchingPrefix,omitempty" validate:"omitempty"`
-	Priority  SINT   `json:"priority,omitempty validate:"required_if=Type SRV"`
-	Weight    SINT   `json:"weight,omitempty validate:"required_if=Type SRV""`
-	Port      SINT   `json:"port,omitempty validate:"required_if=Type SRV""`
+	Priority  SINT   `json:"priority,omitempty" validate:"required_if=Type SRV"`
+	Weight    SINT   `json:"weight,omitempty" validate:"required_if=Type SRV"`
+	Port      SINT   `json:"port,omitempty" validate:"required_if=Type SRV"`
 	Protocol  string `json:"protocol,omitempty" validate:"required_if=Type SRV"`
 	Service   string `json:"service,omitempty" validate:"required_if=Type SRV"`
 	Target    string `json:"target,omitempty" validate:"required_if=Type SRV"`

version.go
@@ -1,3 +1,3 @@
 package main
 
-const dcTemplateLinterVersion uint = 87
+const dcTemplateLinterVersion uint = 88