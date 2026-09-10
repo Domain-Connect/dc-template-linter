Domain-Connect/dc-template-linter

commit 8cfdcb138b04cf2760285a0128f7544c9e66bc93

add new option --merge-or-fail
* make all error / warn / info fail (I think this functionality is there already
* check if there are any bare variables in host or pointsTo and fail if there are
* fail if syncPubKeyDomain is not set

Requested-by: Pawel Kowalik <kowalik@denic.de>
Signed-off-by: Sami Kerola <kerolasa@iki.fi>

internal/version.go
@@ -1,3 +1,3 @@
 package internal
 
-const ProjectVersion uint = 114
+const ProjectVersion uint = 115

libdctlint/record.go
@@ -241,6 +241,14 @@ func (conf *Conf) checkRecord(
 		})
 	}
 
+	if conf.mergeOrFail && (checkBareVariables(record.PointsTo) || checkBareVariables(record.Host)) {
+		pointsTo := record.PointsTo
+		host := record.Host
+		exitVal |= conf.emit(rlog, internal.DCTL1040, func(e *zerolog.Event) *zerolog.Event {
+			return e.Str("pointsTo", pointsTo).Str("host", host)
+		})
+	}
+
 	// DNS provider specific checks
 	if conf.cloudflare {
 		if record.Essential != "" {
@@ -424,4 +432,29 @@ func checkSPFRules(conf *Conf, rules string, rlog zerolog.Logger) exitvals.Check
 	}
 
 	return exitVal
+}
+
+func checkBareVariables(s string) bool {
+	if s == "" {
+		return false
+	}
+
+	i := 0
+	for i < len(s) {
+		// Each segment must start with %
+		if s[i] != '%' {
+			return false
+		}
+
+		// Find the closing %
+		end := strings.Index(s[i+1:], "%")
+		if end == -1 {
+			return false
+		}
+
+		// Move past this variable (end is relative to i+1, so add 2 for both % chars)
+		i += end + 2
+	}
+
+	return true
 }

internal/dctl.go
@@ -67,6 +67,7 @@ const (
 	DCTL1037 DCTL = 1037
 	DCTL1038 DCTL = 1038
 	DCTL1039 DCTL = 1039
+	DCTL1040 DCTL = 1040
 
 	DCTL5000 DCTL = 5000
 	DCTL5001 DCTL = 5001
@@ -136,6 +137,7 @@ var dctlToString = map[DCTL]string{
 	DCTL1037: "hostRequired template should be combined with NS or CNAME record that uses host @ or empty",
 	DCTL1038: "APEXCNAME and REDIRxxx records are not widely supported",
 	DCTL1039: "all records use the same variable as suffix, consider using host parameter instead",
+	DCTL1040: "bare variables in host or pointsTo record field",
 
 	// cloudflare messages
 	DCTL5000: "syncBlock is not supported",
@@ -205,6 +207,7 @@ var dctlLevel = map[DCTL]zerolog.Level{
 	DCTL1037: zerolog.InfoLevel,
 	DCTL1038: zerolog.InfoLevel,
 	DCTL1039: zerolog.InfoLevel,
+	DCTL1040: zerolog.ErrorLevel,
 
 	// cloudflare messages
 	DCTL5000: zerolog.ErrorLevel,

libdctlint/config.go
@@ -23,6 +23,7 @@ type Conf struct {
 	collision   map[string]bool
 	duplicates  map[uint64]bool
 	checkLogos  bool
+	mergeOrFail bool
 	cloudflare  bool
 	inplace     bool
 	increment   bool
@@ -53,6 +54,11 @@ func (c *Conf) SetLogger(l zerolog.Logger) *Conf {
 
 func (c *Conf) SetCheckLogos(b bool) *Conf {
 	c.checkLogos = b
+	return c
+}
+
+func (c *Conf) SetMergeOrFail(b bool) *Conf {
+	c.mergeOrFail = b
 	return c
 }
 

main.go
@@ -48,6 +48,7 @@ func main() {
 	}
 	checkLogos := flag.Bool("logos", false, "check logo urls are reachable (requires network)")
 	cloudflare := flag.Bool("cloudflare", false, "use Cloudflare specific template rules")
+	mergeOrFail := flag.Bool("merge-or-fail", false, "the https://github.com/Domain-Connect/Templates auto-merge condition")
 	inplace := flag.Bool("inplace", false, "inplace write back pretty-print")
 	indent := flag.Uint("indent", 4, "number of spaces in an indent step of the pretty json")
 	increment := flag.Bool("increment", false, "increment template version, useful when pretty-printing")
@@ -62,6 +63,16 @@ func main() {
 	if *version {
 		_, _ = fmt.Printf("dc-template-linter version %d\n", internal.ProjectVersion)
 		os.Exit(0)
+	}
+
+	// Override loglevel and other options when --merge-or-fail is in use
+	if *mergeOrFail {
+		t := true
+		checkLogos = &t
+		tol := "debug"
+		toleration = &tol
+		ll := "info"
+		loglevel = &ll
 	}
 
 	// Runtime init
@@ -82,6 +93,7 @@ func main() {
 		SetCheckLogos(*checkLogos).
 		SetPrettyPrint(*prettyPrint).
 		SetIndent(*indent).
+		SetMergeOrFail(*mergeOrFail).
 		SetCloudflare(*cloudflare)
 
 	if flag.NArg() < 1 {

commit 0e138834b6d18dc580dd7c3990bcf0b7cf2b7ab9

lib: restrict merge if both host and pointsTo are bare variabless
Pawel wanted: 'any bare host/pointsTo too restrictive?  Just limit for
both, but pass if just one is bare?  → yes, only “double bare” shall warrant
manual review'

Requested-by: Pawel Kowalik <kowalik@denic.de>
Reference: https://app.slack.com/client/TBVLY977Y/unified-files/doc/F0AD8NA988L
Fixes: 8cfdcb138b04cf2760285a0128f7544c9e66bc93
Signed-off-by: Sami Kerola <kerolasa@iki.fi>

internal/version.go
@@ -1,3 +1,3 @@
 package internal
 
-const ProjectVersion uint = 121
+const ProjectVersion uint = 122

libdctlint/record.go
@@ -250,7 +250,7 @@ func (conf *Conf) checkRecord(
 		})
 	}
 
-	if conf.mergeOrFail && (checkBareVariables(record.PointsTo) || checkBareVariables(record.Host)) {
+	if conf.mergeOrFail && (checkBareVariables(record.PointsTo) && checkBareVariables(record.Host)) {
 		pointsTo := record.PointsTo
 		host := record.Host
 		exitVal |= conf.emit(rlog, internal.DCTL1040, func(e *zerolog.Event) *zerolog.Event {